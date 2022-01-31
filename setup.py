f4ae3ae06c29e4e3c39a0193d8a190388d408d65
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-01-31 14:23:01 -0500

    build: Iterate the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ocdsextensionregistry',
-    version='0.1.9',
+    version='0.1.10',
     author='Open Contracting Partnership',
     author_email='data@open-contracting.org',
     url='https://github.com/open-contracting/extension_registry.py',
