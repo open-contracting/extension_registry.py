967ee58269479ef677e80b466c5da625b6d482cd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-12-13 18:48:29 -0500

    build: Iterate the version number

--- ocdsextensionregistry/__init__.py
+++ ocdsextensionregistry/__init__.py
@@ -5,6 +5,7 @@ from ocdsextensionregistry.extension import Extension
 from ocdsextensionregistry.extension_registry import ExtensionRegistry
 from ocdsextensionregistry.extension_version import ExtensionVersion
 from ocdsextensionregistry.profile_builder import ProfileBuilder
+from ocdsextensionregistry.versioned_release_schema import get_versioned_release_schema
 
 EXTENSIONS_DATA = 'https://raw.githubusercontent.com/open-contracting/extension_registry/main/extensions.csv'
 EXTENSION_VERSIONS_DATA = 'https://raw.githubusercontent.com/open-contracting/extension_registry/main/extension_versions.csv'
@@ -19,4 +20,5 @@ __all__ = (
     "ExtensionVersion",
     "ProfileBuilder",
     "build_profile",
+    "get_versioned_release_schema",
 )
